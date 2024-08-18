71af98a55296fe0849b64b214b75f0e0f7c5f8d3
flxinxout <giovanni.ranieri@epfl.ch>
2024-08-18 15:47:28 +0200

    new network functions, static addresses added

--- rover_pkg/rover_pkg/network_monitoring.py
+++ rover_pkg/rover_pkg/network_monitoring.py
@@ -1,9 +1,7 @@
 from rclpy.node import Node
 import requests
 from requests.auth import HTTPBasicAuth
-from librouteros import connect
-from librouteros.login import plain
-import json
+import json, time
 from ping3 import ping
 
 class NetworkMonitoring(Node):
@@ -13,68 +11,66 @@ class NetworkMonitoring(Node):
         
         self.rover_state = rover_state
         self.subnet = '169.254.55'
-        self.url_connected_device = 'http://169.254.55.1/rest/interface/wifi/registration-table'
-        self.url_dhcp_server = 'http://169.254.55.1/rest/ip/'
+        self.url_connected_device = f"http://{self.subnet}.1/rest/interface/wifi/registration-table"
         self.auth = HTTPBasicAuth('admin', 'XploreAntenna3')
-        self.list_ips = []
+        self.devices_connected = []
+        self.wireless_connection = None
         
         with open("/home/xplore/dev_ws/src/rover_pkg/rover_pkg/ip_names.json") as json_file:
             self.ip_names = dict(json.load(json_file))
 
-        self.connectivity_cs = self.create_timer(1.0, self.timer_callback)
-        self.ping_timer = self.create_timer(10.0, self.check_devices_in_subnet)
+        self.wireless_devices_timer = self.create_timer(2.0, self.get_wireless_devices)
+        self.ping_cs_timer = self.create_timer(2.0, self.ping_cs)
+        self.check_static_devices_timer = self.create_timer(5.0, self.check_static_devices)
+        
         self.get_logger().info("Networking Node ready")
     
 
-    def ping_device(self, ip):
-        responses = [ping(ip, timeout=1) for _ in range(3)]
+    def ping_cs(self):
+        responses = [ping(f"{self.subnet}.{2}", timeout=1) for _ in range(2)]
+        cs = False
         success_responses = [r for r in responses if r is not None]
-        return len(success_responses) >= 2
+        if len(success_responses) >= 1 and self.wireless_connection is not None:
+            for idx, device in enumerate(self.wireless_connection):
+                if(device.get('mac-address') == "D4:01:C3:DC:B9:77"):
+                    self.rover_state['rover']['network']['signal_strength'] = device.get('signal')
+                    cs = True
+                    break
+            
+            if not cs:
+                self.rover_state['rover']['network']['signal_strength'] = '0.0' # mean that the cs is not in the wireless tab
 
-    def check_devices_in_subnet(self, subnet):
-        for i in range(2, 255):
-            ip = f"{subnet}.{i}"
-            if self.ping_device(ip):
-                if ip not in self.list_ips:
-                    self.list_ips.append(ip)     
+    def ping_static_address(self, ip):
+        responses = [ping(ip, timeout=1) for _ in range(2)]
+        success_responses = [r for r in responses if r is not None]
+        return len(success_responses) >= 1
+
+    def check_static_devices(self):
+        for ip, name in self.ip_names.items():
+            if self.ping_static_address(ip):
+                # device connected
+                if ip not in self.devices_connected:
+                    self.devices_connected.append(ip)     
                     name = self.ip_names[ip]
-                    if name is not None:   
-                        item = {
+                    item = {
                             "name": name,
-                            "ip": ip
-                        }
-                    else:
-                        item = {
                             "ip": ip
                         }
                     
                     self.rover_state['rover']['network']['connected_devices'].append(item)
             
             else:
-                if ip in self.list_ips:
+                if ip in self.devices_connected:
                     # remove the device with this ip in the list
-                    self.rover_state['rover']['network']['connected_devices'][:] = 
-                    [device for device in self.rover_state['rover']['network']['connected_devices'] if 
+                    self.rover_state['rover']['network']['connected_devices'][:] = [device for device in self.rover_state['rover']['network']['connected_devices'] if 
                      device.get("ip") == ip] 
-                    self.list_ips.remove(ip)
+                    self.devices_connected.remove(ip)
 
-
-    def timer_callback(self):
-        response = requests.get(self.url_connected_device, auth=self.auth, verify=False)
-        cs = False
+    def get_wireless_devices(self):
+        response = requests.get(self.url_connected_device, auth=self.auth, verify=False)        
         if response.status_code == 200:
-            data = response.json()
-            if len(data) != 0:
-                for idx, device in enumerate(data):
-                    if(device.get('mac-address') == "D4:01:C3:DC:B9:77"):
-                        self.rover_state['rover']['network']['signal_strength'] = device.get('signal')
-                        cs = True
-                        break
-                
-                if not cs:
-                    self.rover_state['rover']['network']['signal_strength'] = '0.0'
-            else:
-                self.rover_state['rover']['network']['signal_strength'] = '1.0'
+            self.wireless_connection = response.json()
+            
         else:
-            self.rover_state['rover']['network']['signal_strength'] = '2.0'
+            self.wireless_connection = None
         
