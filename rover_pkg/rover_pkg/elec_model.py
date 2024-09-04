467eea6ae89a6ac33b667ca2d54d7de1c1507dd8
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2024-09-04 20:00:59 +0200

    update elec interfaces

--- rover_pkg/rover_pkg/elec_model.py
+++ rover_pkg/rover_pkg/elec_model.py
@@ -1,4 +1,4 @@
-from custom_msg.msg import LedsCommand, Led
+from custom_msg.msg import LedsCommand, Led, MassArray, FourInOne, Voltage
 class Elec:
     def __init__(self, rover_node, model):
         self.rover_node = rover_node
@@ -8,6 +8,18 @@ class Elec:
 
         self.leds = self.rover_node.node.create_publisher(LedsCommand, 
                                                              self.rover_node.el_names["/**"]["ros__parameters"]["el_pubsub_led_commands"], 1)
+        
+        self.rover_node.node.create_subscription(MassArray, 
+                                                  self.rover_node.el_names["/**"]["ros__parameters"]["elec_drill_mass"], self.drill_mass_callback, 1)
+        
+        self.rover_node.node.create_subscription(MassArray,
+                                                    self.rover_node.el_names["/**"]["ros__parameters"]["elec_container_mass"], self.container_mass_callback, 1)
+        
+        self.rover_node.node.create_subscription(FourInOne,
+                                                    self.rover_node.el_names["/**"]["ros__parameters"]["elec_fourinone_sensor"], self.four_in_one_callback, 1)
+        
+        self.rover_node.node.create_subscription(Voltage,
+                                                    self.rover_node.el_names["/**"]["ros__parameters"]["elec_voltage_sensor"], self.voltage_callback, 1)
 
     def send_led_commands(self, subsystem, mode):
 
@@ -42,7 +54,21 @@ class Elec:
         command.leds = [led]
         self.leds.publish(command)
 
-    def update_mass_measurement(self, msg):
-        self.rover_node.rover_state_json['electronics']['sensors']['mass_sensor']["drill"] = msg.mass[1]
-        # self.rover_node.rover_state_json['electronics']['sensors']['mass_sensor']["drill"] = msg.mass[1]
-        # self.rover_node.rover_state_json['electronics']['sensors']['mass_sensor']["drill"] = msg.mass[1]
+    def drill_mass_callback(self, msg):
+        self.rover_node.rover_state_json['electronics']['sensors']['mass_sensor']["mass_drill"] = msg.mass[1]
+    
+    def container_mass_callback(self, msg):
+        self.rover_node.rover_state_json['electronics']['sensors']['mass_sensor']["mass_container"] = msg.mass[0]
+    
+    def four_in_one_callback(self, msg):
+        self.rover_node.rover_state_json['electronics']['sensors']['four_in_one'] = {
+            "temperature": msg.temperature,
+            "moisture": msg.moisture,
+            "conductivity": msg.conductivity,
+            "ph": msg.ph
+        }
+    
+    def voltage_callback(self, msg):
+        self.rover_node.rover_state_json['electronics']['sensors']['voltmeter'] = {
+            "voltage": msg.voltage
+        }
