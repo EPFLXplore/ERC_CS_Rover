93ec226f47d1edd3ef30b669fccd7696ba080dfb
flxinxout <giovanni.ranieri@epfl.ch>
2024-10-18 13:37:58 +0200

    interface topic refactor for EL. Need to test now with the rover.

--- rover_pkg/rover_pkg/elec_model.py
+++ rover_pkg/rover_pkg/elec_model.py
@@ -1,4 +1,4 @@
-from custom_msg.msg import LedsCommand, Led, MassArray, FourInOne, Voltage
+from custom_msg.msg import LedsCommand, Led, MassArray, FourInOne, Voltage, Imu, Mag
 class Elec:
     def __init__(self, rover_node, model):
         self.rover_node = rover_node
@@ -19,6 +19,13 @@ class Elec:
                                                     self.rover_node.el_names["FOUR_IN_ONE_TOPIC"], self.four_in_one_callback, 1)
 
         self.rover_node.node.create_subscription(Voltage, self.rover_node.el_names["VOLTAGE_TOPIC"], self.voltage_callback, 1)
+
+        self.rover_node.node.create_subscription(Imu, self.rover_node.el_names['IMU_TOPIC'], self.imu_callback, 1)
+
+        self.rover_node.node.create_subscription(Mag, self.rover_node.el_names['MAG_RAW_TOPIC'], self.mag_callback, 1)
+
+        self.rover_node.node.create_subscription(Mag, self.rover_node.el_names['POTENTIOMETER_TOPIC'], self.potentiometer_callback, 1)
+
 
     def send_led_commands(self, subsystem, mode):
 
@@ -58,10 +65,10 @@ class Elec:
         self.leds.publish(command)
 
     def drill_mass_callback(self, msg):
-        self.rover_node.rover_state_json['electronics']['sensors']['mass_sensor']["mass_drill"] = msg.mass[1]
+        self.rover_node.rover_state_json['electronics']['sensors']['mass_sensors']["mass_drill"] = msg.mass[1]
     
     def container_mass_callback(self, msg):
-        self.rover_node.rover_state_json['electronics']['sensors']['mass_sensor']["mass_container"] = msg.mass[0]
+        self.rover_node.rover_state_json['electronics']['sensors']['mass_sensors']["mass_container"] = msg.mass[0]
     
     def four_in_one_callback(self, msg):
         self.rover_node.rover_state_json['electronics']['sensors']['four_in_one'] = {
@@ -75,3 +82,26 @@ class Elec:
         self.rover_node.rover_state_json['electronics']['sensors']['voltmeter'] = {
             "voltage": msg.voltage
         }
+    
+    def imu_callback(self, msg):
+        self.rover_node.rover_state_json['electronics']['sensors']['imu'] = {
+            "xyzw": msg.orientation,
+            "orientation_covariance": msg.imu.orientation_cov,
+            "ang_vel_cov": msg.imu.angular_velocity_covariance,
+            "linear_accel_cov": msg.imu.linear_acceleration_covariance,
+            "ang_vel": msg.imu.angular_velocity,
+            "linear_accel": msg.imu.linear_acceleration
+        }
+    
+    def mag_callback(self, msg):
+        self.rover_node.rover_state_json['electronics']['sensors']['magnetic'] = {
+            "mag_field": msg.mag_raw.magnetic_field,
+            "mag_field_cov": msg.mag_raw.magnetic_field_covariance
+        }
+    
+    def potentiometer_callback(self, msg):
+        self.rover_node.rover_state_json['electronics']['sensors']['potentiometer'] = {
+            "position": 
+        }
+    
+
